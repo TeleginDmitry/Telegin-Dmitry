e76799a1279e0ad8a54453e4b5e53c7b2e9bf38a
d1mas1k2022 <d1mas1k2KA@yandex.ru>
2023-12-25 21:55:33 +0300

    created services for technologies

---
+++ src/scripts/technologies.ts
@@ -0,0 +1,15 @@
+import { technologiesService } from "services/technologies/technologies.service";
+
+const skillsListElement = document.querySelector(".skills__list");
+
+new Promise((resolve, reject) => {
+  const service = new technologiesService();
+  service.get().then((technologies) => {
+    technologies.forEach(({ name, color }) => {
+      skillsListElement?.insertAdjacentHTML(
+        "beforeend",
+        `<li class="skill"><span style="background-color: ${color}"></span>${name}</li>`,
+      );
+    });
+  });
+});

210f2b897ce67502208c6f783d431a14f540ba3e
d1mas1k2022 <d1mas1k2KA@yandex.ru>
2024-01-10 21:44:46 +0300

    Data without query on the server

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
 import "./assets/styles/mixins.scss";
 
 import "./scripts/swiper";
-import "./scripts/technologies";
-import "./scripts/projects";
 import "./scripts/sendEmail";
 import "./scripts/sidebar";

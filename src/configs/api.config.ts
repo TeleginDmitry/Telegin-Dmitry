2c4e6b2d1597e6150e66d774653e60e52bc5370e
Telegin Dmitry <dtelegindev@gmail.com>
2025-04-25 11:15:14 +0300

    feature/gallery

--- src/configs/api.config.ts
+++ src/configs/api.config.ts
@@ -1,5 +1,2 @@
-export const API_URL = 'http://localhost:5000/api/'
-export const STATIC_URL = 'http://localhost:5000/uploads/'
-
-// export const API_URL = 'https://telegin-dmitry-server.vercel.app/api/'
-// export const STATIC_URL = 'https://telegin-dmitry-server.vercel.app/uploads/'
+export const API_URL = 'https://telegin-dmitry-server.vercel.app/api/'
+export const STATIC_URL = 'https://telegin-dmitry-server.vercel.app/uploads/'
